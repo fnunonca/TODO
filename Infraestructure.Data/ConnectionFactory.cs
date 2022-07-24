7d9758690e0d6f2f511f6c08877cf7466571b5bf
Fernando Vasquez <fnunonca@gmail.com>
2022-07-24 01:18:44 -0500

    add connectionFactory

---
+++ Infraestructure.Data/ConnectionFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Transversal.Common;
+
+namespace Infrastructure.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection
+                {
+                    ConnectionString = _configuration.GetConnectionString("BD_Todo")
+                };
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+        }
+    }
+}
---
+++ Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Transversal.Common
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
